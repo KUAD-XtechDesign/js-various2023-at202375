1a53e7d8114cc92ef5243f84d583331f254c8c6d
at202375 <138648433+at202375@users.noreply.github.com>
2023-11-03 21:00:07 +0900

    雪９

--- sample_04/js/script04.js
+++ sample_04/js/script04.js
@@ -9,23 +9,20 @@ const creatSnow = () => {
   mixSize = 10;
 
   /*雪の大きさをランダムに決める　*/
-  letsnowSaiz = Math.random() * (maxSize - minSize) +minSiza;
+  let snowSaiz = Math.random() * (maxSize - minSize) +minSiza;
 
-
-  snow.style.width = snowSize + "px"
-  snow.style.height = snowSize + "px"
+  snow.style.width = snowSaiz + "px"
+  snow.style.height = snowSaiz + "px"
 
   /*雪の降り始めの位置を決定する　*/
   snow.left = Math.random() * 100 + "%"
 
-  snowContaimer.appendchild(snow);
+  snowcontainer.appendchild(snow);
 
   /* 10秒後に雪を消す */
   setTimeout(() => {
-  snow.remove();
-
-
- },10000);
+     snow.remove();
+  },10000);
 };
 
 setInterval(creatSnow, 100);
